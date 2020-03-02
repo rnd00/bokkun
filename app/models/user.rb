15aa1e50cce779383d8356d873f7d5b92b36eb61
Adil Omary <adilomary@hotmail.fr>
2020-03-02 17:57:35 +0900

    add/edit user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :job_title, presence: true
+  validates :manager, inclusion: {in: [true, false]}
 end
