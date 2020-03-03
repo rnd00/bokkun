e2f585c98e30e2ba687efa68547e18aae0bc134a
Mike Warren <mikeywarren89@gmail.com>
2020-03-03 14:50:33 +0900

    allowed any user to see landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#landing'
+  root to: 'pages#landing'
   get '/dashboard', to: 'users#dashboard', as: :dashboard
   resources :receipts, only: :show do
     resources :receipt_items, only: [:new, :create]
