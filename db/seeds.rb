e0ffba975219eaf14a9fa3ac9c0a149a48b16ae4
Ryanda Diedan <rndmx00@gmail.com>
2020-03-03 10:55:45 +0900

    trip done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def budget_gen(n, a)
+  Budget.create!( name: n, amount: a)
+end
+
 
 puts "destroy_all everything..."
 Budget.destroy_all
@@ -24,14 +28,14 @@ User.create!( email: "admin@bokkun.me",
   last_name: "bokkun",
   job_title: "admin",
   manager: true)
-manager_user = User.create!(
+uemura = User.create!(
   email: "manager@bokkun.me",
   password: "123123",
   first_name: "Uemura",
   last_name: "Mitsuo",
   job_title: "Sales Rep",
   manager: true)
-employee_user = User.create!(
+yamada = User.create!(
   email: "employee@bokkun.me",
   password: "123456",
   first_name: "Yamada",
@@ -39,5 +43,35 @@ employee_user = User.create!(
   job_title: "Sales Rep",
   manager: false)
 puts "done with user generation!"
+
+
+puts "generating 2 trips and budgets..."
+tokyo = Trip.create!(
+  name: "Tokyo Trip",
+  destination: "Tokyo, Japan",
+  purpose: "First Contact",
+  customer: "Adil Omary",
+  start_date: Date.today - 2,
+  end_date: Date.today - 1)
+fukuoka = Trip.create!(
+  name: "Fukuoka Trip",
+  destination: "Fukuoka, Japan",
+  purpose: "Currying favor",
+  customer: "Mike Warren",
+  start_date: Date.today,
+  end_date: Date.today + 2)
+
+tbudget = Budget.create!(
+  name: Meal)
+puts "done with trip & budget generation!"
+
+puts "connecting trip-user..."
+tu1 = TripUser.create!(
+  user: uemura,
+  trip: tokyo)
+tu2 = TripUser.create(
+  user: yamada,
+  trip: fukuoka)
+puts "trip-user connected!"
 
 
