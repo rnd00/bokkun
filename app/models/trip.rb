e2fd382312657f5489102a4a1a62c6e0be95d6fd
Brian Heise <bnheise@gmail.com>
2020-03-04 10:59:05 +0900

    updated trip and trip_budget model with methods to access totals and to set trip budget's remaining amount upon creation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,4 +6,12 @@ class Trip < ApplicationRecord
   has_many :budgets, through: :trip_budgets
 
   validates :name, :destination, :purpose, :customer, :start_date, :end_date, presence: true
+
+  def total_budget
+    self.budgets.reduce(0) { |total, budget| total + budget.amount }
+  end
+
+  def total_remaining
+    self.trip_budgets.reduce(0) { |total, trip_budget| total + trip_budget.remaining_amount }
+  end
 end
--- app/models/trip_budget.rb
+++ app/models/trip_budget.rb
@@ -3,4 +3,9 @@ class TripBudget < ApplicationRecord
   belongs_to :budget
 
   validates :remaining_amount, numericality: { only_integer: true }
+
+  def save
+    self.remaining_amount = self.budget.amount if self.remaining_amount.nil?
+    super
+  end
 end
