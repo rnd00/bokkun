8450de5c743df5e0f69c835ecc7f690a2ca37ccb
Brian Heise <bnheise@gmail.com>
2020-03-05 11:01:51 +0900

    added total spent method to trip_budgets to show the total of all receipts

--- app/models/trip_budget.rb
+++ app/models/trip_budget.rb
@@ -2,4 +2,8 @@ class TripBudget < ApplicationRecord
   belongs_to :trip
   belongs_to :budget
   has_many :receipts
+
+  def total_spent
+    self.receipts.reduce(0) { |total, receipt| total + receipt.total_amount }
+  end
 end
