8d9b82e012bf484ca44047c3c857d6cba2a31b6f
Brian Heise <bnheise@gmail.com>
2020-03-05 11:12:58 +0900

    budget bars for each category display properly

--- app/models/trip_budget.rb
+++ app/models/trip_budget.rb
@@ -3,7 +3,11 @@ class TripBudget < ApplicationRecord
   belongs_to :budget
   has_many :receipts
 
-  def total_spent
-    self.receipts.reduce(0) { |total, receipt| total + receipt.total_amount }
+  def total_remaining
+    self.budget.amount - self.receipts.reduce(0) { |total, receipt| total + receipt.total_amount }
+  end
+
+  def budget_percent
+    ((self.total_remaining.to_f / self.budget.amount.to_f).round(2) * 100).to_i
   end
 end
