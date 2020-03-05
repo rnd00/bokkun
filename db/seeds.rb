1769117566ff7f42d1fd173de9dbc396e61ac38f
Ryanda Diedan <rndmx00@gmail.com>
2020-03-05 10:51:58 +0900

    add more generators to make trip simpler

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ============================================================================
+# SHORTHAND HELPER
+# ============================================================================
 
-# begin generators
+##
+# some of the functions below dependent on this (these) shorthand(s)
+
+today = Date.today
+
+# ============================================================================
+# SKELETON GENERATOR FUNCTIONS
+# ============================================================================
 
 def user_gen(email, fname, lname, job, manager_status)
   User.create!(
@@ -15,7 +18,7 @@ def user_gen(email, fname, lname, job, manager_status)
     first_name: fname,
     last_name: lname,
     job_title: job,
-    manager: manager_status)
+    manager: manager_status )
 end
 
 def trip_gen(name, dest, purpose, customer, sdate, edate)
@@ -25,7 +28,7 @@ def trip_gen(name, dest, purpose, customer, sdate, edate)
   purpose: purpose,
   customer: customer,
   start_date: sdate,
-  end_date: edate)
+  end_date: edate )
 end
 
 def receipt_gen(company, total, date, tax, user, trip_budget)
@@ -35,8 +38,7 @@ def receipt_gen(company, total, date, tax, user, trip_budget)
     date: date,
     tax_amount: tax,
     user: user,
-    trip_budget: trip_budget
-    )
+    trip_budget: trip_budget )
 end
 
 def budget_gen(name, amount)
@@ -48,40 +50,68 @@ end
 def trip_budget_gen(budget, trip)
   TripBudget.create!(
     trip: trip,
-    budget: budget)
+    budget: budget )
 end
 
-# template generator
+# ============================================================================
+# TEMPLATE GENERATOR FUNCTIONS
+# ============================================================================
 
 def temp_budget_gen
-  # call this once only, assign it to a variable
-  # add more type as you need
-  types = [['meal', 15000], ['travel', 20000], ['accomodations', 25000]]
-  budget = []
-  types.each do |type|
+  types = [
+    # add more type as you need
+
+    ['food', 15000],
+    ['travel', 20000],
+    ['miscellaneous', 20000],
+    ['accomodation', 25000]]
+  types.map do |type|
     budget << budget_gen(type[0], type[1])
   end
-  budget
+end
+
+def temp_trip_gen(city, purpose, customer, length_of_trip)
+  name = "#{city} trip"
+  dest = "#{city}, Earth"
+  rand_num = rand(length_of_trip)
+  sdate = today - rand_num
+  edate = today + (length_of_trip - rand_num)
+  trip_gen(name, dest, purpose, customer, sdate, edate)
+end
+
+def temp_col_trip_gen(trip_datas)
+  trip_datas.map do |trip|
+    temp_trip_gen(trip[0], trip[1], trip[2], trip[3])
+  end
 end
 
 def temp_trip_budget_gen(temp_budget, trip)
   # call this each time you make a trip
-  temp_budget.each do |budget|
+  temp_budget.map do |budget|
     trip_budget_gen(budget, trip)
   end
 end
 
-today = Date.today
 
-# end generators
+# ============================================================================
+# SAMPLE DATAS
+# ============================================================================
 
-# start sample data
+##
+# Array inside another array;
+# user = [login-email, first-name, last-name, job-title, manager-boolean]
+# trip = []
+
 user_datas = [['segawa@bokkun.me', 'Segawa', 'Taku', 'Branch Manager', true],
               ['hirai@bokkun.me', 'Hirai', 'Kako', 'Sales Rep', false],
               ['ueno@bokkun.me', 'Ueno', 'Keisuke', 'Sales Rep', false]]
 
-# end sample data
+trip_datas = [["Tokyo Trip", "Tokyo, Japan", "First Contact", "Adil Omary", today - 2, today - 1],
+              []]
 
+# ============================================================================
+# DESTROY ALL
+# ============================================================================
 
 puts "destroy_all everything..."
 # join tables first then the data table
@@ -93,6 +123,10 @@ TripBudget.destroy_all
 Trip.destroy_all
 User.destroy_all
 puts "done with destroy_all!"
+
+# ============================================================================
+# GENERATE USERS
+# ============================================================================
 
 puts "generating default users..."
 User.create!( email: "admin@bokkun.me",
@@ -108,9 +142,11 @@ yamada = user_gen("yamada@bokkun.me", "Yamada", "Taro", "Sales Rep", false)
 user_datas.each do |data|
   user_gen(data[0], data[1], data[2], data[3], data[4])
 end
-
 puts "done with user generation!"
 
+# ============================================================================
+# GENERATE TRIP, BUDGETS, AND THEIR JOIN TABLES
+# ============================================================================
 
 puts "generating 2 trips and budgets..."
 temp_budget = temp_budget_gen()
@@ -145,6 +181,10 @@ tu5 = TripUser.create(
   user: uemura,
   trip: izumo)
 puts "trip-user connected!"
+
+# ============================================================================
+# GENERATE RECEIPTS
+# ============================================================================
 
 puts "generating 2 receipts..."
 sukiya = receipt_gen("Sukiya", "2000", today, 10, yamada, fukuoka.trip_budgets.first)
