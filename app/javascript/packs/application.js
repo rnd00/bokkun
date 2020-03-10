6cb3e77ca91fdd41954293769d42074c6c0180e8
Mike Warren <mikeywarren89@gmail.com>
2020-03-10 13:03:30 +0900

    added select2 to employeees and fixed budget displpay to allowances

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,12 @@ import "bootstrap";
 import { toggleReceipt } from '../receiptEmployer/receipt-ticket';
 import { employeeNavbarToggle } from '../employeeNavbar/toggle';
 
+// Select2 requirements
+import {initSelect2} from '../plugins/init_select2'
+import 'select2/dist/css/select2.css';
+
+
+initSelect2();
 
 employeeNavbarToggle();
 
