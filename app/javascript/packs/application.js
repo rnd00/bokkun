63086567b86375c4014cc8345da0d817e2ae71ac
Mike Warren <mikeywarren89@gmail.com>
2020-03-10 11:34:07 +0900

    added flatpickr to all dates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { toggleReceipt } from '../receiptEmployer/receipt-ticket';
 import { employeeNavbarToggle } from '../employeeNavbar/toggle';
+import '../plugins/flatpickr'
 
 
 employeeNavbarToggle();
