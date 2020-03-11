93da489dff11e36d2ec1e769bef141e32906f9d6
Brian Heise <bnheise@gmail.com>
2020-03-11 10:10:28 +0900

    added js to allow photos to auto submit after adding from receipt show page for employee

--- app/javascript/plugins/collapse_toggle.js
+++ app/javascript/plugins/collapse_toggle.js
@@ -1,15 +1,16 @@
-
 const collapseToggle = () => {
   const collapse = document.getElementById("collapse-link");
-  collapse.addEventListener("click", () => {
-    const toggler = document.getElementById("trip-toggle");
-      setTimeout(() => {
-        if (toggler.innerHTML.includes('down')){
-          toggler.innerHTML = "<i class='fas fa-caret-up display-4 text-white-50'></i>"
-        } else {
-          toggler.innerHTML = "<i class='fas fa-caret-down display-4 text-white-50'></i>"
-        }
-      }, 200);
-  });
+  if (collapse) {
+    collapse.addEventListener("click", () => {
+      const toggler = document.getElementById("trip-toggle");
+        setTimeout(() => {
+          if (toggler.innerHTML.includes('down')){
+            toggler.innerHTML = "<i class='fas fa-caret-up display-4 text-white-50'></i>"
+          } else {
+            toggler.innerHTML = "<i class='fas fa-caret-down display-4 text-white-50'></i>"
+          }
+        }, 200);
+    });
+  }
 };
 export { collapseToggle };
