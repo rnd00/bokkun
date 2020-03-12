1fa919013a067a134583df5bf8c8faae66640102
Ryanda Diedan <rndmx00@gmail.com>
2020-03-12 20:33:40 +0900

    hotfix dashboard, should be correct now

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,8 @@
 class Budget < ApplicationRecord
   has_many :trip_budgets, dependent: :destroy
   has_many :trips, through: :trip_budgets
+  has_many :receipts, through: :trip_budgets
+  has_many :receipt_items, through: :receipts
 
   validates :name, :amount, presence: true
   validates :amount, numericality: { only_integer: true }
